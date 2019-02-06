509d20b0c91a423deb5fadcb694d6ae0177f5762
BigBrosh <maksuhin@gmail.com>
2019-02-06 07:37:25 +0200

    - added modify user in UserController.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -13,8 +13,16 @@ function getUserById(userId) {
   return users.filter(({ id }) => id === userId)[0];
 }
 
+function modifyUser(id, name, param) {
+  const user = users.find(user => user.id === id);
+  user[name] = param;
+
+  return user;
+}
+
 export {
   listUsers,
   listUsersByOrganizationId,
-  getUserById
+  getUserById,
+  modifyUser
 };
